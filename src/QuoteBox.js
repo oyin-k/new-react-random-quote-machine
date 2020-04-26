191340f9ef089fa1ffc77e0f3f674f0458f0ff6d
Oyinkan Oyetunmibi <oyink7@gmail.com>
2020-04-26 02:36:45 +0100

    WIP: markup and styling done with quote icon bg, random quote api, height overflowing viewport

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.scss";
 
 const QuoteBox = () => {
+  const [quote, setQuote] = useState(
+    "Whatever the mind of man can conceive and believe, it can achieve."
+  );
+  const [author, setAuthor] = useState("Napoleon Hill");
+
   return (
     <div className="app" id="quote-box">
-      <h1>Random Quote Machine</h1>
+      <div className="quote">
+        <p id="text">{quote}</p>
+      </div>
+      <div className="quote-info-and-actions">
+        <div className="new-quote-cta-box">
+          <button id="new-quote">Get new quote</button>
+          <a href="https://twitter.com/intent/tweet" id="tweet-quote">
+            Tweet this quote
+          </a>
+        </div>
+        <div className="quote-author" id="author">
+          <img src="" alt="" className="author-image" />
+          <p className="author-name">{author}</p>
+          {/* <span className="author-field" ></span> */}
+        </div>
+      </div>
     </div>
   );
 };
